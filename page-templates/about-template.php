8a05ef2c2b9bbd79e7af12f018b29588842efc02
alirezajahan13 <107421412+alirezajahan13@users.noreply.github.com>
2023-12-03 18:21:44 +0330

    cart , account , checkout , aboutUs

---
+++ page-templates/about-template.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: About Template
+*/
+get_header();
+?>
+
+<div class="aboutUsPageBack">
+    <div class="aboutUsPage mainView highMargined">
+        <div class="generalHeading">
+            <h1>درباره ما</h1>
+        </div>
+        <div class="aboutPageContent highPadding lowRadius mediumMargined">
+            <div class="sectionOne">
+                <div class="textBox">
+                    <h3>لورم ایپسوم متن ساختگی</h3>
+                    <p class="justified">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+                </div>
+                <div class="imageBox">
+                    <img class="lowRadius" src="<?php echo get_template_directory_uri()?>/img/about-sample.jpg" alt="">
+                </div>
+            </div>
+            <div class="sectionTwo centeredText">
+                <div class="textBox">
+                    <h3>لورم ایپسوم متن ساختگی</h3>
+                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+                </div>
+            </div>
+            <div class="sectionOne differentOrderAbout">
+                <div class="textBox">
+                    <h3>لورم ایپسوم متن ساختگی</h3>
+                    <p class="justified">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است</p>
+                </div>
+                <div class="imageBox">
+                    <img class="lowRadius" src="<?php echo get_template_directory_uri()?>/img/about-sample.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
